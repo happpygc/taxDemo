0a07f469f53a72bef96eca74a23bdddbc018abd2
gc <18616517276@163.com>
2016-11-22 21:25:34 +0800

    10-22-21.25

---
+++ src/com/shenji/test/TestMerge.java
@@ -0,0 +1,14 @@
+package com.shenji.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMerge {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
